6f635748583d0289491087ba24c0ddfbdc852bff
FerJRL <frl711m@uma.es>
2024-02-11 22:56:12 +0100

    trimmed fat

--- README.md
+++
@@ -1,5 +0,0 @@
-# elrastro-producto
-
-El rastro backend microservicios para productos
-
-https://backend-producto-e7ciurv34-valentins-projects-78f7db39.vercel.app/
--- v2.mjs
+++ v2.mjs
@@ -2,7 +2,7 @@ import { ObjectId } from "mongodb";
 import products from "./conn.mjs";
 import express from "express";
 import { getFiltros, getSortByDate } from "./help.mjs";
-import { getClientById, getBidsByProductId } from "./api.mjs";
+import { getClientById } from "./api.mjs";
 import axios from "axios";
 
 const app = express.Router();
@@ -17,21 +17,6 @@ app.get("/", async (req, res) => {
   const sort = getSortByDate(req);
   const results = await products.find(filtros).sort(sort).toArray();
   res.send(results).status(200);
-});
-
-app.get("/:id1/:id2", async (req, res) => {
-  try {
-    const results = await products.findOne({
-      $or: [
-        { userID: req.params.id1, soldID: req.params.id2 },
-        { userID: req.params.id2, soldID: req.params.id1 },
-      ],
-    });
-
-    res.send(results).status(200);
-  } catch (e) {
-    res.send(e).status(500);
-  }
 });
 
 app.delete("/:id", async (req, res) => {
